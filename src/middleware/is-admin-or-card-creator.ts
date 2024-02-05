446f047f7f2e16e71bcbd740f6a76c71ed16c15d
yamkatz <139678031+yamkatz@users.noreply.github.com>
2024-02-05 11:30:00 +0200

    Update is-admin-or-card-creator.ts

--- src/middleware/is-admin-or-card-creator.ts
+++ src/middleware/is-admin-or-card-creator.ts
@@ -1,17 +1,18 @@
-import { RequestHandler, Request } from "express";
+import { RequestHandler } from "express";
 import { auth } from "../service/auth-service";
 import { User } from "../database/model/user";
+import { Card } from "../database/model/card";
 import { extractToken } from "./is-admin";
 import { BizCardsError } from "../error/biz-cards-error";
 import { Logger } from "../logs/logger";
 
-const isAdminOrUser: RequestHandler = async (req, res, next) => {
+const isAdminOrCardCreator: RequestHandler = async (req, res, next) => {
   try {
     const { id } = req.params;
     const token = extractToken(req);
     const { email } = auth.verifyJWT(token);
 
-    //get user from database:
+    // Get user from the database:
     const user = await User.findOne({ email });
 
     if (!user) {
@@ -19,22 +20,33 @@ const isAdminOrUser: RequestHandler = async (req, res, next) => {
       throw new BizCardsError("User does not exist", 401);
     }
 
-    if (id == user.id) return next();
+    // Check if the user is an admin
+    if (user.isAdmin) {
+      return next();
+    }
 
-    if (user.isAdmin) return next();
+    // Check if the user is the creator of the card
+    const card = await Card.findOne({ _id: id, userId: user._id });
 
-    Logger.error("Permission Denied", 401, {
-      message: "Only admin/The id must belong to the user",
+    if (card) {
+      return next(); // User is the creator of the card
+    }
+
+    // If not an admin and not the card creator, deny permission
+    Logger.error("Permission Denied", 403, {
+      message: "You are not an admin or not the card creator",
       userId: user._id,
-      requestedUserId: id,
+      cardId: id,
     });
-
-    res
-      .status(401)
-      .json({ message: "Only admin/The id must belong to the user" });
+    res.status(403).json({
+      error: "Permission Denied, you are not an admin or not the card creator",
+    });
   } catch (e) {
+    Logger.error("Error in isAdminOrCardCreator middleware", 500, e);
     next(e);
   }
 };
+
+export { isAdminOrCardCreator };
 
 export { isAdminOrUser };
